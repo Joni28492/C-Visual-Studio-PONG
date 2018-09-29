eda6aa2ab3f896abb68e58fd0ce44f44d16c465a
Joni28492 <43235263+Joni28492@users.noreply.github.com>
2018-09-29 14:29:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PONG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbPalaDos = new System.Windows.Forms.PictureBox();
+            this.pbLineaIntermedia = new System.Windows.Forms.PictureBox();
+            this.pbPelota = new System.Windows.Forms.PictureBox();
+            this.pbPala = new System.Windows.Forms.PictureBox();
+            this.lblNivel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPalaDos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLineaIntermedia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPelota)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPala)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.pbPalaDos);
+            this.panel1.Controls.Add(this.pbPelota);
+            this.panel1.Controls.Add(this.pbPala);
+            this.panel1.Controls.Add(this.lblNivel);
+            this.panel1.Controls.Add(this.pbLineaIntermedia);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(898, 572);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbPalaDos
+            // 
+            this.pbPalaDos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pbPalaDos.Location = new System.Drawing.Point(852, 187);
+            this.pbPalaDos.Name = "pbPalaDos";
+            this.pbPalaDos.Size = new System.Drawing.Size(25, 149);
+            this.pbPalaDos.TabIndex = 4;
+            this.pbPalaDos.TabStop = false;
+            // 
+            // pbLineaIntermedia
+            // 
+            this.pbLineaIntermedia.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pbLineaIntermedia.Location = new System.Drawing.Point(436, 12);
+            this.pbLineaIntermedia.Name = "pbLineaIntermedia";
+            this.pbLineaIntermedia.Size = new System.Drawing.Size(10, 548);
+            this.pbLineaIntermedia.TabIndex = 3;
+            this.pbLineaIntermedia.TabStop = false;
+            // 
+            // pbPelota
+            // 
+            this.pbPelota.BackColor = System.Drawing.Color.Yellow;
+            this.pbPelota.Location = new System.Drawing.Point(631, 248);
+            this.pbPelota.Name = "pbPelota";
+            this.pbPelota.Size = new System.Drawing.Size(30, 30);
+            this.pbPelota.TabIndex = 2;
+            this.pbPelota.TabStop = false;
+            // 
+            // pbPala
+            // 
+            this.pbPala.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pbPala.Location = new System.Drawing.Point(17, 187);
+            this.pbPala.Name = "pbPala";
+            this.pbPala.Size = new System.Drawing.Size(25, 149);
+            this.pbPala.TabIndex = 1;
+            this.pbPala.TabStop = false;
+            // 
+            // lblNivel
+            // 
+            this.lblNivel.AutoSize = true;
+            this.lblNivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNivel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNivel.Location = new System.Drawing.Point(321, 9);
+            this.lblNivel.Name = "lblNivel";
+            this.lblNivel.Size = new System.Drawing.Size(241, 55);
+            this.lblNivel.TabIndex = 0;
+            this.lblNivel.Text = "<<0 - 0>>";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 572);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPalaDos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLineaIntermedia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPelota)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPala)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbLineaIntermedia;
+        private System.Windows.Forms.PictureBox pbPelota;
+        private System.Windows.Forms.PictureBox pbPala;
+        private System.Windows.Forms.Label lblNivel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbPalaDos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PONG
+{
+    public partial class Form1 : Form
+    {
+        private int paradas = 0;
+        private int velocidad = 15;
+        private int nivel = 1;
+        private bool direccionArriba;
+        private bool direccionIzq;
+        private int puntUno = 0;
+        private int puntDos = 0;
+        Random r = new Random(DateTime.Now.Millisecond);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            //movimiento de pala con teclas 
+            switch (e.KeyCode)
+            {
+                //el movimiento lo hago de esta manera para cuando 
+                //el nivel aumente no llegue al punto de no puedo 
+                //alcanzar la pelota
+                case Keys.Up:
+
+
+                    if (pbPala.Top <= panel1.Top)
+                        pbPala.Top = panel1.Top; //no se salga por arriba
+                    else
+                        pbPala.Top -= 2*velocidad;
+                    break;
+
+                case Keys.Down:
+
+                    if (pbPala.Bottom >= panel1.Bottom)
+                    {
+                        pbPala.Top = panel1.Height - pbPala.Height;//no se sale por abajo
+                    }
+                    else
+                        pbPala.Top += 2 * velocidad;
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            nuevaPelota();
+        }
+            
+        //funcion nuevaPelota
+        private void nuevaPelota()
+        {
+            //pelota se posiciona aleatoriamente de la mitad hacia la derecha 
+     
+            pbPelota.Left = r.Next(panel1.Width / 2, panel1.Width - pbPelota.Width);
+            pbPelota.Top = r.Next(0, panel1.Height);
+
+            if (pbPelota.Left / 2 == 0)
+                direccionArriba = true;
+            else
+                direccionArriba = false;
+           
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //direccion vertical 
+            if (direccionArriba)
+                pbPelota.Top -= velocidad;
+            else
+                pbPelota.Top += velocidad;
+            if (pbPelota.Top <= panel1.Top)
+                direccionArriba = false;
+            else if (pbPelota.Bottom >= panel1.Bottom)
+                direccionArriba = true;
+
+
+
+            //direccion horizontal 
+            if (direccionIzq)
+                pbPelota.Left -= velocidad;
+            else
+                pbPelota.Left += velocidad;
+
+
+
+            
+
+
+
+
+
+            //choque panel derecha
+            if (pbPelota.Right >= panel1.Right)
+            {
+                puntUno++;
+                pbPelota.Left = panel1.Width / 2;
+                direccionIzq = true;
+                lblNivel.Text = "marca jugador 1";
+            }
+            //chocar con pared izquierda 
+            else if (pbPelota.Left <= panel1.Left) {
+                puntDos++;
+                pbPelota.Left = panel1.Width / 2;
+                direccionIzq = false; 
+                lblNivel.Text = "marca jugador 2";
+            }
+
+            //colision en pala izquierda
+            else if (
+                // llega a la altura de la pal y vuelve
+                pbPelota.Left <= pbPala.Right
+                //para el margen superior y coge tambien los 3/4 de la pelota para cuando no choeque entera
+                && pbPelota.Top > pbPala.Top - (3 * pbPelota.Height) / 4
+                //para el margen inferior
+                && pbPelota.Bottom < ((pbPala.Top + pbPala.Height) + (3 * pbPelota.Height) / 4)
+                )
+            {
+                direccionIzq = false;
+                paradas++;
+                if (paradas % 10 == 0)
+                {
+                    velocidad += 5;
+                    nivel++;
+                }
+
+               
+            }
+
+            //colision en pala derecha
+            else if(
+                //llega a la altura de la pala y vuelve 
+                pbPelota.Right>= pbPalaDos.Left
+                //para el margen superior
+                && pbPelota.Top > pbPalaDos.Top - (3 * pbPelota.Height) / 4
+                //para el margen inferior
+                && pbPelota.Bottom < ((pbPalaDos.Top + pbPalaDos.Height) + (3 * pbPelota.Height) / 4)
+                )
+            {
+                direccionIzq = true;
+                paradas++;
+
+                if (paradas % 10 == 0)
+                {
+                    velocidad += 5;
+                    nivel++;
+                }
+
+            }
+
+
+
+            //velocidad mayor o igual que el ancho de la pala
+            if (velocidad >= pbPala.Width)
+            {
+                //hacemos la pelota mas pequeña 
+                pbPelota.Width -= (velocidad - pbPala.Width);
+                pbPelota.Height -= (velocidad - pbPala.Width);
+
+                //hacemos que no se haga mas pequeña de 5x5 para que no desaparezca 
+                if (pbPelota.Height <= 5)
+                {
+                    pbPelota.Height = 5;
+                    pbPelota.Width = 5;
+
+                }
+            }
+
+
+            //inteligencia artificial de la pala 
+            //captura la diferencia que hay entre la pelota y la mitad de la pala
+            //y se intenta ajustar a la misma
+            //se vuelve un poco loco cuando hay mucha distancia 
+
+            if(pbPelota.Top < pbPalaDos.Top)
+            {
+
+                pbPalaDos.Top -= ((pbPelota.Top + pbPelota.Height) - (pbPalaDos.Height / 2));
+                //no salirse por arriba
+                if (pbPalaDos.Top <= panel1.Top)
+                    pbPalaDos.Top = panel1.Top;
+                
+            }
+
+            else if(pbPelota.Top > pbPalaDos.Top + pbPalaDos.Height)
+            {
+
+                pbPalaDos.Top = pbPelota.Top - (pbPalaDos.Top - pbPalaDos.Height / 2);
+                if (pbPalaDos.Bottom >= panel1.Bottom)
+                {
+
+                    pbPalaDos.Top = (panel1.Top + panel1.Height) - pbPalaDos.Height;
+                }
+
+
+            }
+         
+
+            lblNivel.Text = "<<" + puntUno + " - " + puntDos + ">>";
+
+
+        }
+    }
+}
